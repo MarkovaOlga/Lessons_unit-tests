f1271dcbc786cdc130c60adcc0fdd952dee63e92
MarkovaOlga <olgamarkova85@ya.ru>
2023-11-24 11:15:32 +0300

    seminar5 added

--- src/test/java/seminar5/selenium/SeleniumTest2.java
+++ src/test/java/seminar5/selenium/SeleniumTest2.java
@@ -1,9 +1,4 @@
 package seminar5.selenium;
-//1. Открывает главную страницу Google.
-//        2. Вводит "Selenium" в поисковую строку и нажимает кнопку "Поиск в Google".
-//        3. В результатах поиска ищет ссылку на официальный сайт Selenium
-//        (https://www.selenium.dev) и проверяет, что ссылка действительно присутствует среди
-//        результатов поиска.
 
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
@@ -11,16 +6,29 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class SeleniumTest01 {
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SeleniumTest2 {
     @Test
     public void browserTest() throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\Desktop\\GB\\chromedriver-win64\\chromedriver.exe");
         WebDriver webDriver = new ChromeDriver();
         webDriver.get("https://google.com");
         WebElement searchBox = webDriver.findElement(By.name("q"));
-        searchBox.sendKeys("GeekBrains");
+        searchBox.sendKeys("Selenium");
         searchBox.submit();
-        Thread.sleep(10000);
+        List<WebElement> searchResult = webDriver.findElements(By.cssSelector("div"));
+        boolean isFind = false;
+        for (WebElement element: searchResult) {
+            if (element.getText().contains("selenium.dev")) {
+                isFind = true;
+                break;
+            }
+        }
         webDriver.quit();
+        assertTrue(isFind);
     }
+
 }
